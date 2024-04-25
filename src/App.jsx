ce062212e465505c8e09eb0e0334be3eafc7c198
Mostafa-Ashraff <mostafaashrafwork@gmail.com>
2024-04-25 16:44:54 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Model from "./components/Model";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
     <Highlights />
     <Features />
     <Model />
+    <Footer />
   </main>
   )
 };
